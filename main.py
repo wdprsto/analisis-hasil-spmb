6a5b93230c9e25bf2bd3490dad1a4baee7a53793
Wahyu Dwi Prasetio <wdprsto@gmail.com>
2023-06-11 23:06:52 +0700

    Membuat boilerplate untuk dashboard streamlit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+
+sns.set(style='dark')
+
+# Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
+
+
+# Load cleaned data
+
+
+# Filter data
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.svg")
+    
+
+# # Menyiapkan berbagai dataframe
+
