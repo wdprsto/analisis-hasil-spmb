f05d90c186899fb78eb8600a6afe391b9939cd45
Wahyu Dwi Prasetio <wdprsto@gmail.com>
2023-07-24 07:11:00 +0700

    making all the figure blue

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 from numerize import numerize
 from helper import set_bar_pcnt, rainbow_text
 from matplotlib import transforms
-import plotly.express as px
+from matplotlib.lines import Line2D
 
 st.set_page_config(page_title="Analisis SPMB STIS 2022 - Wahyu",
                    page_icon="📈",
@@ -122,23 +122,93 @@ with cola2:
 colb1, colb2 = st.columns(2)
 
 with colb1:
-    st.dataframe(pg_skd.set_index('lokasi.formasi'))
+    st.dataframe(pg_skd,
+                 column_config={
+                    "lokasi.formasi": "Lokasi Formasi",
+                    "nilai_min_skd": "Minimum SKD",
+                    "nilai_max_skd": "Maksimum SKD",
+                    "nilai_rerata_skd": "Rerata SKD",
+                    # "stars": st.column_config.NumberColumn(
+                    #     "Github Stars",
+                    #     help="Number of stars on GitHub",
+                    #     format="%d ⭐",
+                    # ),
+                    # "url": st.column_config.LinkColumn("App URL"),
+                    # "views_history": st.column_config.LineChartColumn(
+                    #     "Views (past 30 days)", y_min=0, y_max=5000
+                    # ),
+                },
+                hide_index=True,
+                use_container_width=True
+            )
 
 with colb2:
-    st.dataframe(pg_mtk.set_index('lokasi.formasi'))
+    st.dataframe(pg_mtk,                 
+                 column_config={
+                    "lokasi.formasi": "Lokasi Formasi",
+                    "nilai_min_mtk": "Minimum MTK",
+                    "nilai_max_mtk": "Maksimum MTK",
+                    "nilai_rerata_mtk": "Rerata MTK",
+                },
+                hide_index=True,
+                use_container_width=True
+                 )
 
 "- Adanya kuota formasi per Provinsi yang diatur oleh BPS"
 
 "---"
 
-colc1, colc2 = st.columns([4,1])
+colc1, colc2 = st.columns([3,2])
 
 with colc1:
-    fig = plt.figure(figsize=(6, 4))
-    ax = sns.countplot(data=df_eda, y='nama.pendidikan', order=df_eda['nama.pendidikan'].value_counts().index)
-    ax.set(xlabel='Jumlah Pendaftar', ylabel='Tingkat Pendidikan')
+    label = df_eda['nama.pendidikan'].value_counts()
+    fig, ax1 = plt.subplots(figsize=(7, 2.65), # width, height in inches
+                        dpi=110)           # resolution of the figure
+    ax1.invert_yaxis()
+    # tune the subplot layout by setting sides of the figure
+    fig.subplots_adjust(left=0.445, right=0.765, top=0.77, bottom=0.01)
 
-    st.pyplot(fig)
+    # draw horizontal colored bars
+    bars = ax1.barh(range(len(label)),
+                    label.tolist(),
+                    height=0.65,
+                    color=[BLUE2] + [BLUE3] + [BLUE4]*3)
+    
+    # set properties for axes object (ticks for all issues with labels)
+    plt.setp(ax1, yticks=np.arange(len(label)), yticklabels=['SMA IPA', 'MA IPA', 'SMA IPS',
+       'SMK TIK', 'MA IPS'])
+
+    # change the appearance of ticks, tick labels, and gridlines
+    ax1.tick_params(bottom='off', left='off', labelbottom='off')
+
+    # configure y tick label appearance
+    for item in ax1.get_yticklabels():
+        item.set_fontsize(7)
+        item.set_color(GRAY3)
+
+        offset = transforms.ScaledTranslation(3, 0, fig.dpi_scale_trans)
+        item.set_transform(item.get_transform() + offset)
+
+    
+    # add numerical data labels for each bar
+    for i, b in enumerate(bars):
+        plt.text(b.get_width()+(300,-300)[i==0], b.get_y() + b.get_height()*0.6,
+                str(label.tolist()[i]),
+                fontsize=6,
+                color=('black','white')[i==0],
+                horizontalalignment=('left','right')[i==0])
+    
+    # remove chart border
+    for spine in ax1.spines.values():
+        spine.set_visible(False)
+
+    # remove x tick
+    ax1.tick_params(axis='both', which='both', length=0, labelbottom=False)
+
+    ax1.text(-3500, -1, 'Sebaran Pendidikan Peserta', fontsize=10, color=GRAY1)
+
+    st.pyplot(fig,
+              use_container_width=True)
 
 with colc2:
     st.markdown('<div style="font-size:40px;font-weight:bold;">Lantas?</div>', unsafe_allow_html=True)
@@ -184,15 +254,51 @@ with cold11:
         )
     
     "Latar Belakang Pendidikan Peserta yang Lulus"
-    st.dataframe(df_eda[df_eda['hasil.akhir_x']=='Lulus']['nama.pendidikan'].value_counts().reset_index().rename(columns={"nama.pendidikan":"jenjang pendidikan"}))
+    st.dataframe(
+        df_eda[df_eda['hasil.akhir_x']=='Lulus']['nama.pendidikan'].value_counts().reset_index(),
+        column_config={
+            "index": "Tingkat Pendidikan",
+            "nama.pendidikan": "Jumlah Lulusan",
+        },
+        hide_index=True,
+        use_container_width=True
+                 
+        )
 
 
 
 with cold21:
     "### Distribusi Nilai SKD Nasional"
     fig = plt.figure(figsize=(6, 5))
-    ax = sns.kdeplot(df_eda_01, x='skd.nilai', hue="hasil.akhir_x")
-    ax.set(xlabel='Nilai SKD', ylabel='Kepadatan')
+    ax = sns.kdeplot(df_eda_01, x='skd.nilai', hue="hasil.akhir_x",)
+
+    ax.set(xlabel='', ylabel='')
+    ax.lines[0].set_color(BLUE2)
+    ax.lines[1].set_color(GRAY9)
+
+    ytick_labs = np.arange(0, ax.lines[1].get_ydata().max()+0.001, 0.001)
+
+    plt.setp(ax,
+         yticks=ytick_labs,
+         yticklabels=[f"{round(y*100,2)}%" for y in ytick_labs]
+         )
+    
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color(GRAY5)
+    ax.spines['bottom'].set_color(GRAY5)
+    
+    legend_elements = [Line2D([0], [0], color=GRAY9, lw=3, label='Tidak Lulus'),
+                       Line2D([0], [0], color=BLUE2, lw=3, label='Lulus',
+                              markerfacecolor='g', markersize=15),
+                    ]
+    ax.legend(handles=legend_elements, loc='upper left')
+
+    for item in ax.get_xticklabels():
+        item.set_color(GRAY3)
+    for item in ax.get_yticklabels():
+        item.set_color(GRAY3)
+    
     st.pyplot(fig)
 
     "Peserta yang lulus cenderung memiliki nilai SKD yang lebih tinggi dibandingkan dengan yang tidak lolos. Hal ini terlihat dari puncak orange berada di sebelah kanan dari distribusi grafik biru."
@@ -202,8 +308,14 @@ cold12, cold22 = st.columns(2)
 
 with cold12:
     "### Korelasi Secara Nasional"
-    fig = plt.figure(figsize=(6, 5))
-    ax = sns.heatmap(korelasi, annot=True, fmt=".2f");
+    fig = plt.figure(figsize=(5, 5))
+    ax = sns.heatmap(korelasi, 
+                     annot=True, 
+                     fmt=".2f",
+                    yticklabels=['Nilai SKD','Nilai MTK', 'Hasil'],
+                    xticklabels=['Nilai SKD','Nilai MTK', 'Hasil'],
+                    cmap="Blues");
+    ax.collections[0].colorbar.remove()
     st.pyplot(fig)
 
     "Plot korelasi terhadap atribut SKD, Matematika, dan Hasil Akhir menunjukkan bahwa SKD memiliki korelasi yang lemah dengan atribut Hasil Seleksi. Di sisi lain, ditemukan bahwa tes Matematika memiliki korelasi yang sedang dengan Hasil Seleksi."
@@ -212,7 +324,34 @@ with cold22:
     "### Distribusi Nilai MTK Nasional"
     fig = plt.figure(figsize=(6,5))
     ax = sns.kdeplot(df_eda_01, x='mtk.nilai', hue="hasil.akhir_x")
-    ax.set(xlabel='Nilai Matematika', ylabel='Kepadatan')
+    
+    ax.set(xlabel='', ylabel='')
+    ax.lines[0].set_color(BLUE2)
+    ax.lines[1].set_color(GRAY9)
+
+    ytick_labs = np.arange(0, ax.lines[1].get_ydata().max()+0.001, 0.001)
+
+    plt.setp(ax,
+         yticks=ytick_labs,
+         yticklabels=[f"{round(y*100,2)}%" for y in ytick_labs]
+         )
+    
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color(GRAY5)
+    ax.spines['bottom'].set_color(GRAY5)
+    
+    legend_elements = [Line2D([0], [0], color=GRAY9, lw=3, label='Tidak Lulus'),
+                       Line2D([0], [0], color=BLUE2, lw=3, label='Lulus',
+                              markerfacecolor='g', markersize=15),
+                    ]
+    ax.legend(handles=legend_elements, loc='upper left')
+
+    for item in ax.get_xticklabels():
+        item.set_color(GRAY3)
+    for item in ax.get_yticklabels():
+        item.set_color(GRAY3)
+    
     st.pyplot(fig)
 
     "Peserta yang lulus memiliki nilai tes Matematika yang lebih besar dibandingkan dengan yang tidak lolos. Hal ini terlihat dari puncak orange berada di sebelah kanan dari distribusi grafik biru. Perbedaan nilai yang paling sering muncul ini bahkan menyentuh angka 50 poin."
@@ -246,10 +385,37 @@ cold10, cold20 = st.columns(2)
 
 with cold10:
     "### Jumlah Peserta Lulus"
-    fig = plt.figure(figsize=(6, 5))
-    ax=sns.countplot(data = df_eda_01[df_eda_01['lokasi.formasi']==pilihan_lokasi], x = "hasil.akhir_x")
-    ax.set(xlabel='Hasil Akhir', ylabel='Jumlah')
-    st.pyplot(fig)
+
+    label = df_eda_01[df_eda_01['lokasi.formasi']==pilihan_lokasi]['hasil.akhir_x'].value_counts()
+
+    fig, ax1 = plt.subplots(figsize=(7.45, 3.5), dpi=110)
+    fig.subplots_adjust(left=0.154, right=0.77, top=0.89, bottom=0.1)
+    bars = plt.bar([0, 1], [label[0], label[1]],
+                color=[BLUE5, BLUE1],
+                linewidth=0.5,
+                width=0.55)
+    ax1.tick_params(bottom=False, left=False, labelleft=False, labelbottom=True)
+
+    ax1.spines['left'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['top'].set_visible(False)
+
+    plt.xticks([0, 1], ['Tidak Diterima', 'Diterima'])
+    ax1.spines['bottom'].set_color(GRAY9)
+    for item in ax1.get_xticklabels():
+        item.set_fontsize(12)
+        item.set_color(GRAY4)
+
+    for i, bar in enumerate(bars):
+        plt.text(bar.get_x() + 0.2,
+                bar.get_height()+(-10,10)[i==1],
+                label[i],
+                fontsize=12,
+                verticalalignment=('top','bottom')[i==1],
+                color=('white','black')[i==1])
+
+    st.pyplot(fig,
+                use_container_width=True)
 
 
 with cold20:
@@ -264,19 +430,11 @@ with cold20:
     cold100, cold101 = st.columns(2)
 
     with cold100:
-        st.metric(label="Tidak Diterima",
-        value=f"{ditolak_prov[0]}"
-        )
-
         st.metric(label="Keketatan Provinsi",
         value=f"{numerize.numerize(keketatan_prov['hasil.akhir_x'])}%"
         )
 
     with cold101:
-        st.metric(label="Diterima",
-        value=f"{diterima_prov[0]}"
-        )
-
         st.metric(label="Diterima Lk",
         value=f"{diterima_prov_lk[0]}"
         )
@@ -296,20 +454,92 @@ with cole11:
     skd_prov = df_eda_01[df_eda_01['lokasi.formasi']==pilihan_lokasi]
     fig = plt.figure(figsize=(6, 5))
     ax = sns.kdeplot(skd_prov, x='skd.nilai', hue="hasil.akhir_x")
-    ax.set(xlabel='Nilai SKD', ylabel='Kepadatan')
+    ax.set(xlabel='', ylabel='')
+    ax.lines[0].set_color(BLUE2)
+    ax.lines[1].set_color(GRAY9)
+
+    ytick_labs = np.arange(0, ax.lines[1].get_ydata().max()+0.001, 0.001)
+
+    plt.setp(ax,
+         yticks=ytick_labs,
+         yticklabels=[f"{round(y*100,2)}%" for y in ytick_labs]
+         )
+    
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color(GRAY5)
+    ax.spines['bottom'].set_color(GRAY5)
+    
+    legend_elements = [Line2D([0], [0], color=GRAY9, lw=3, label='Tidak Lulus'),
+                       Line2D([0], [0], color=BLUE2, lw=3, label='Lulus',
+                              markerfacecolor='g', markersize=15),
+                    ]
+    ax.legend(handles=legend_elements, loc='upper left')
+
+    for item in ax.get_xticklabels():
+        item.set_color(GRAY3)
+    for item in ax.get_yticklabels():
+        item.set_color(GRAY3)
+    
     st.pyplot(fig)
 
-    st.dataframe(pg_skd[pg_skd['lokasi.formasi']==pilihan_lokasi].set_index('lokasi.formasi'))
+    st.dataframe(
+        pg_skd[pg_skd['lokasi.formasi']==pilihan_lokasi],
+        column_config={
+            "lokasi.formasi": "Lokasi Formasi",
+            "nilai_min_skd": "Minimum SKD",
+            "nilai_max_skd": "Maksimum SKD",
+            "nilai_rerata_skd": "Rerata SKD",
+        },
+        hide_index=True,
+        use_container_width=True
+           
+        )
 
 with cole21:
     "### Distribusi Nilai MTK"
     mtk_prov = df_eda_01[df_eda_01['lokasi.formasi']==pilihan_lokasi]
     fig = plt.figure(figsize=(6, 5))
     ax = sns.kdeplot(mtk_prov, x='mtk.nilai', hue="hasil.akhir_x")
-    ax.set(xlabel='Nilai Matematika', ylabel='Kepadatan')
+    ax.set(xlabel='', ylabel='')
+    ax.lines[0].set_color(BLUE2)
+    ax.lines[1].set_color(GRAY9)
+
+    ytick_labs = np.arange(0, ax.lines[1].get_ydata().max()+0.001, 0.001)
+
+    plt.setp(ax,
+         yticks=ytick_labs,
+         yticklabels=[f"{round(y*100,2)}%" for y in ytick_labs]
+         )
+    
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color(GRAY5)
+    ax.spines['bottom'].set_color(GRAY5)
+    
+    legend_elements = [Line2D([0], [0], color=GRAY9, lw=3, label='Tidak Lulus'),
+                       Line2D([0], [0], color=BLUE2, lw=3, label='Lulus',
+                              markerfacecolor='g', markersize=15),
+                    ]
+    ax.legend(handles=legend_elements, loc='upper left')
+
+    for item in ax.get_xticklabels():
+        item.set_color(GRAY3)
+    for item in ax.get_yticklabels():
+        item.set_color(GRAY3)
+    
     st.pyplot(fig)
 
-    st.dataframe(pg_mtk[pg_mtk['lokasi.formasi']==pilihan_lokasi].set_index('lokasi.formasi'))
+    st.dataframe(pg_mtk[pg_mtk['lokasi.formasi']==pilihan_lokasi].set_index('lokasi.formasi'),
+                column_config={
+                    "lokasi.formasi": "Lokasi Formasi",
+                    "nilai_min_mtk": "Minimum MTK",
+                    "nilai_max_mtk": "Maksimum MTK",
+                    "nilai_rerata_mtk": "Rerata MTK",
+                },
+                hide_index=True,
+                use_container_width=True
+                )
 
 
 cole12, cole22 = st.columns(2)
@@ -317,8 +547,15 @@ cole12, cole22 = st.columns(2)
 with cole12:
     "### Korelasi"
     korelasi_prov = df_eda_01[df_eda_01['lokasi.formasi']==pilihan_lokasi][num_col].corr()
-    fig = plt.figure(figsize=(6, 5))
-    ax = sns.heatmap(korelasi_prov, annot=True, fmt=".2f");
+    fig, ax = plt.subplots(figsize=(5,5))
+    sns.heatmap(korelasi_prov, 
+                     annot=True, 
+                     fmt=".2f",
+                    yticklabels=['Nilai SKD','Nilai MTK', 'Hasil'],
+                    xticklabels=['Nilai SKD','Nilai MTK', 'Hasil'],
+                    ax=ax,
+                    cmap="Blues");
+    ax.collections[0].colorbar.remove()
     st.pyplot(fig)
 
 with cole22:
